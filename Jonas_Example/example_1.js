145a80a3cb230b89e861f209bc6c425f4675c127
bhatt-jaymin <bhatt.jaymin@gmail.com>
2023-12-19 13:33:50 +0000

    commit basic variable opration

---
+++ Jonas_Example/example_1.js
@@ -0,0 +1,47 @@
+console.log('hello')
+// JS is High level object-oriented,multi paradigm,programming language.
+/*
+ 1) High level ==> we dont have worry about stuff like memory management
+ 2) Object Oriented ==> based on object for storing some kind of data
+ 3) multi paradigm ==> we can use styles of programming
+ 4) programming language ===> instruct computer to do things
+ 
+ 
+ HTML ===> content [nouns]
+ CSS  ===> Presentation [objective]
+ JavaScript ===> Programming language building web applications [verbs]
+
+ JS
+ 1) dynamic effect & web applications in the browser
+ react angular next & Vue [All are libraries 100% based on JS they might go away but JS not ]
+
+ 2) Web application on web servers [node js]
+
+ 3) Native Mobile Application [React-Native & Ionic]
+
+ 4) Native Desktop Applications [Electron]
+
+
+ */
+
+ /*
+1)
+Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+ */
+let continent=new String("Asia")
+let country=new String ("India")
+let population=new String ("1428.6 million")
+
+let newContinent="Asia"
+let newCountry="India"
+let newPopulation=1428.6 
+/*
+2)
+2. Log their values to the console
+ */
+
+console.log(continent,country,population)
+console.log(`The Continent of ${newContinent} its one Country which name is ${newCountry} total Population is ${newPopulation} million`)
+
+
