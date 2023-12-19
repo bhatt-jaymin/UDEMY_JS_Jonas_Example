c32d6032ecfb5d35106eda4ae34874bb07906fb7
bhatt-jaymin <bhatt.jaymin@gmail.com>
2023-12-19 13:34:20 +0000

    Basic datatype and variable type operation

---
+++ Jonas_Example/example_2.js
@@ -0,0 +1,17 @@
+
+   const isIsland =true
+   let country="Srilanka"
+   let population="14 million"
+   let language=
+
+
+console.log(`on Earth 1990 its ${isIsland} cover with island and ${country} is country with total population ${population}`)
+
+
+
+console.log(typeof isIsland, typeof country, typeof population,typeof language)
+
+language="Sihali"
+population="20 million"
+
+console.log(`on Earth 2022 its ${isIsland} cover with island and ${country} is country with total population ${population} and official native language is ${language}`)
